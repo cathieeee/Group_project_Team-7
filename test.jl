7713ba9b705d3dfcb48b30647424688c036c1bb6
Caleb Brodie <cbrodie2@node1153.oscar.ccv.brown.edu>
2022-04-24 20:51:51 -0400

    parameter gets 0 results but this does work!

--- test.jl
+++ test.jl
@@ -18,36 +18,25 @@ function create_TA2C_from_file(filename::String = ".keys")::Dict
         keys[key] = strip(value)
       end
     end
-
     return keys
 end
-
 TA2c = create_TA2C_from_file()
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 # these lines create query parameters in the form of a dictionary and a url link to the twitter API
 # recent_tweet_counts
 # https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Recent-Tweet-Counts/recent_tweet_counts.py
-
 query_params = Dict(
-  "query"=>"ivermectin",
-  "granularity"=>"day",
+  "query"=>"from:twitterdev -is:retweet #ivermectin"
 )
-search_url = "https://api.twitter.com/2/tweets/search/counts/recent"
-
+search_url = "https://api.twitter.com/2/tweets/search/recent"
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 # full-archive-search
 # https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Full-Archive-Search/full-archive-search.py
-
-_query_params = Dict(
- "query"=>"(from:twitterdev -is:retweet) OR #ivermectin",
- "tweet.fields"=>"author_id",
- "tweet.fields" =>"ivermectin",
- "tweet.fields" =>"hydroxychloroquine",
- "tweet.fields" =>"remdesivir"
-)
-_search_url = "https://api.twitter.com/2/tweets/search/30day/fullarchive"
-
+#_query_params = Dict(
+# "query"=>"(from:twitterdev -is:retweet) lang:en ("ivermectin" OR "hydroxychloroquine" OR "remdesivir")",
+# "tweet.fields"=>"author_id",
+#)
+#_search_url = https://api.twitter.com/2/tweets/search/30day/fullarchive
 # make_GET_req
 # Blanket function for simple GET requests. Returns full request object.
 #
@@ -55,10 +44,9 @@ _search_url = "https://api.twitter.com/2/tweets/search/30day/fullarchive"
 # --------------------------------------------------
 # url    :: "https://api.twitter.com/2/tweets/search/30day/fullarchive"
 # params :: Dictionary of parameters to send/ body of request
-
 ####### need to make params dictionary ######
-url = "https://api.twitter.com/2/tweets/search/30day/fullarchive"
-params = _query_params
+url = search_url
+params = query_params
 
 function make_GET_req(url::String, params::Dict)::HTTP.Response
     response = HTTP.request("GET", url, [
@@ -70,8 +58,6 @@ function make_GET_req(url::String, params::Dict)::HTTP.Response
   
     return response
 end
-
 r1 = make_GET_req(search_url, query_params)
-r2 = make_GET_req(_search_url, _query_params)
-
-# need to make a dictionionary of tweet criteria
+#r2 = make_GET_req(_search_url, _query_params)
+println(r1)
