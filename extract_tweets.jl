40ea1525f388f7394a5e2308624efe8aec45e733
cbrodie1 <caleb_brodie@brown.edu>
2022-05-05 17:12:21 -0400

    updated query params to include start/end time, max result, and academic access query params and url

--- extract_tweets.jl
+++ extract_tweets.jl
@@ -51,7 +51,10 @@ module TweetExtractor
     ####### if we get academic access use the below instead
     query_params_academic = Dict(
       "query"=>"((Ivermectin OR Remdesivir OR Hydroxychloroquine OR ivermectin OR remdesivir OR hydroxychloroquine OR #Ivermectin OR #Remdesivir OR #Hydroxychloroquine OR #ivermectin OR #remdesivir OR #hydroxychloroquine) -is:retweet lang:en)",
-      "tweet.fields"=>"text")
+      "tweet.fields"=>"text"
+      "max_results" => "500"
+      "start_time" => "2022-4-1T13:00:00.00Z"
+      "end_time" => "2022-4-30T13:00:00.00Z")
 
     search_url_academic = "https://api.twitter.com/2/tweets/search/all"
 
